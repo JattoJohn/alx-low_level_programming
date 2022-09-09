f833934bae9317f1902e8556442aebdbf285047f
Jatto John <jattojohnovie@gmail.com>
2022-09-09 06:26:58 -0700

    a program that prints the alphabet in lowercase, followed by a new line except q and e.

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase
- * followed by a new line.
+ * main - prints the alphabet in lowercase,
+ * followed by a new line, except q and e
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -12,11 +12,11 @@ int main(void)
 	while (ch <= 'z')
 	{
 		if (ch != 'e' && ch != 'q')
-		{	
+		{
 		putchar(ch);
 		}
 		ch++;
 	}
 	putchar('\n');
-	Return (0);
+	return (0);
 }
