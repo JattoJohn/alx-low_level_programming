413c92da6559423fdb9e9077bda500cfddfbeba8
Jatto John <jattojohnovie@gmail.com>
2022-09-13 08:45:02 -0700

    prints 10 times the alphabet, in lowercase,

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,13 +13,13 @@ void print_alphabet_x10(void)
 
 	while (i < 10)
 	{
-		ch = "a";
-		while (ch "<= z")
+		ch = 'a';
+		while (ch <= 'z')
 		{
-			_putchar("ch");
+			_putchar(ch);
 			ch++;
 		}
-		_putchar("\n");
+		_putchar('\n');
 		i++;
 	}
 }
